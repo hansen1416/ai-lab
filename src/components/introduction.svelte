278ec142603a804e2e5222917272856f6d43998a
hansen1416 <badapplesweetie@gmail.com>
2025-04-03 18:46:59 +0100

    1

--- src/components/introduction.svelte
+++ src/components/introduction.svelte
@@ -3,59 +3,59 @@
 <div>
 	<div class="top-bar">
 		<div class="title">Brighton AI Robotics (AIR) Force Projects</div>
-		<div class="nav button-64"><span class="text">Projects</span></div>
-		<div class="nav button-64"><span class="text">Team</span></div>
-		<div class="nav button-64"><span class="text">Join Us</span></div>
+		<div class="nav button-64"><a class="text">Projects</a></div>
+		<div class="nav button-64"><a class="text">Team</a></div>
+		<div class="nav button-64"><a class="text">Join Us</a></div>
 	</div>
 </div>
 
 <style lang="scss">
-	.button-64 {
-		align-items: center;
-		background-image: linear-gradient(
-			144deg,
-			#af40ff,
-			#5b42f3 50%,
-			#00ddeb
-		);
-		border: 0;
-		border-radius: 8px;
-		box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
-		box-sizing: border-box;
-		color: #ffffff;
-		display: flex;
-		font-family: Phantomsans, sans-serif;
-		font-size: 20px;
-		justify-content: center;
-		line-height: 1em;
-		max-width: 100%;
-		min-width: 140px;
-		padding: 3px;
-		text-decoration: none;
-		user-select: none;
-		-webkit-user-select: none;
-		touch-action: manipulation;
-		white-space: nowrap;
-		cursor: pointer;
-	}
+	// .button-64 {
+	// 	align-items: center;
+	// 	background-image: linear-gradient(
+	// 		144deg,
+	// 		#af40ff,
+	// 		#5b42f3 50%,
+	// 		#00ddeb
+	// 	);
+	// 	border: 0;
+	// 	border-radius: 8px;
+	// 	box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
+	// 	box-sizing: border-box;
+	// 	color: #ffffff;
+	// 	display: flex;
+	// 	font-family: Phantomsans, sans-serif;
+	// 	font-size: 20px;
+	// 	justify-content: center;
+	// 	line-height: 1em;
+	// 	max-width: 100%;
+	// 	min-width: 140px;
+	// 	padding: 3px;
+	// 	text-decoration: none;
+	// 	user-select: none;
+	// 	-webkit-user-select: none;
+	// 	touch-action: manipulation;
+	// 	white-space: nowrap;
+	// 	cursor: pointer;
+	// }
 
-	.button-64:active,
-	.button-64:hover {
-		outline: 0;
-	}
+	// .button-64:active,
+	// .button-64:hover {
+	// 	outline: 0;
+	// }
 
-	.button-64 span {
-		background-color: rgb(5, 6, 45);
-		padding: 16px 24px;
-		border-radius: 6px;
-		width: 100%;
-		height: 100%;
-		transition: 300ms;
-	}
+	// .button-64 span {
+	// 	background-color: rgb(5, 6, 45);
+	// 	padding: 16px 24px;
+	// 	border-radius: 6px;
+	// 	width: 100%;
+	// 	height: 100%;
+	// 	transition: 300ms;
+	// }
 
-	.button-64:hover span {
-		background: none;
-	}
+	// .button-64:hover span {
+	// 	background: none;
+	// }
 
 	$font-white: #fff;
 	.top-bar {
