99b3dbfcb2a981269a35007e7125f7f3cb410191
hansen1416 <badapplesweetie@gmail.com>
2025-04-04 20:42:16 +0100

    1

--- src/components/introduction.svelte
+++ src/components/introduction.svelte
@@ -6,32 +6,35 @@
 		<div class="nav"><a href="#team">Team</a></div>
 		<div class="nav"><a href="#joinus">Join Us</a></div>
 	</div>
-	<div class="description">
-		<div class="title">Brighton AI Robotics (AIR) Force Projects</div>
-		<div class="content">
-			<p>Where code meets cognition and machines learn to move.</p>
+	<div class="desc">
+		<div class="desc-inner">
+			<div class="title">Brighton AI Robotics (AIR) Force Projects</div>
+			<div class="content">
+				<p>Where code meets cognition and machines learn to move.</p>
 
-			<p>
-				The AI & Robotics Lab at the University of Brighton is a
-				cutting-edge research space where artificial intelligence,
-				machine learning, and robotics converge. Focused on real-world
-				impact, the lab develops intelligent systems capable of
-				perception, decision-making, and autonomous behavior. From
-				humanoid robots to swarm robotics, the lab empowers innovation
-				in healthcare, manufacturing, and creative industries.
-			</p>
+				<p>
+					The AI & Robotics Lab at the University of Brighton is a
+					cutting-edge research space where artificial intelligence,
+					machine learning, and robotics converge. Focused on
+					real-world impact, the lab develops intelligent systems
+					capable of perception, decision-making, and autonomous
+					behavior. From humanoid robots to swarm robotics, the lab
+					empowers innovation in healthcare, manufacturing, and
+					creative industries.
+				</p>
 
-			<p>
-				Equipped with state-of-the-art tools, including robotic arms,
-				mobile robots, simulation platforms, and deep learning
-				infrastructure, it serves as a launchpad for pioneering research
-				and student-led breakthroughs.
-			</p>
+				<p>
+					Equipped with state-of-the-art tools, including robotic
+					arms, mobile robots, simulation platforms, and deep learning
+					infrastructure, it serves as a launchpad for pioneering
+					research and student-led breakthroughs.
+				</p>
 
-			<p>
-				Here, machines think, adapt, and evolve. The future is built—not
-				imagined.
-			</p>
+				<p>
+					Here, machines think, adapt, and evolve. The future is
+					built—not imagined.
+				</p>
+			</div>
 		</div>
 	</div>
 </div>
@@ -42,7 +45,7 @@
 	.intro {
 		display: flex;
 		flex-direction: column;
-		width: calc(60vw - 40px);
+		width: calc(50vw - 40px);
 		height: 100%;
 	}
 
@@ -78,11 +81,18 @@
 		}
 	}
 
-	.description {
+	.desc {
 		width: 100%;
 		height: 100%;
-		margin: global.$magin-1;
-		box-sizing: border-box;
+
+		@include global.text-top;
+
+		.desc-inner {
+			@include global.text-bottom;
+
+			box-sizing: border-box;
+			padding: global.$magin-1;
+		}
 
 		.title {
 			font-size: 24px;
@@ -92,8 +102,9 @@
 		.content {
 			display: flex;
 			flex-direction: column;
-			width: 60%;
+			width: 80%;
 			font-size: 20px;
+			line-height: 30px;
 		}
 	}
 </style>
