b105548ac47b8a59299d0dff9242dfe3b749731b
hansen1416 <badapplesweetie@gmail.com>
2025-04-17 16:45:38 +0100

    1

--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -1,14 +1,69 @@
-<script lang="ts"></script>
+<script lang="ts">
+	let tab = $state(1);
+</script>
 
 <div class="projects">
-	<div class="l">
-		<div class="desc">
-			<i> Advancing closer to the future of intelligent systems.</i>
-		</div>
+	<div class="desc">
+		<i> Advancing closer to the future of intelligent systems.</i>
 	</div>
+
 	<div class="project-list">
-		<div class="item">Multi-modal Control for Robotic Devices.</div>
-		<div class="item">Central Artificial Intelligence</div>
+		<div class="tabs">
+			<div class="tab active" class:active={tab === 1}>
+				<button
+					class="tab"
+					onclick={() => {
+						tab = 1;
+					}}
+				>
+					Multi-modal Control for Robotic Devices.
+				</button>
+			</div>
+			<div class="tab" class:active={tab === 2}>
+				<button
+					class="tab"
+					onclick={() => {
+						tab = 2;
+					}}
+				>
+					Central Artificial Intelligence</button
+				>
+			</div>
+		</div>
+		<div class="tab-content" class:active={tab === 1}>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+				eos tempore voluptatum quisquam saepe sed pariatur enim
+				quibusdam asperiores tempora earum quis aliquid repudiandae
+				dolores quaerat voluptate fugiat harum doloremque.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+				eos tempore voluptatum quisquam saepe sed pariatur enim
+				quibusdam asperiores tempora earum quis aliquid repudiandae
+				dolores quaerat voluptate fugiat harum doloremque.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+				eos tempore voluptatum quisquam saepe sed pariatur enim
+				quibusdam asperiores tempora earum quis aliquid repudiandae
+				dolores quaerat voluptate fugiat harum doloremque.
+			</p>
+		</div>
+		<div class="tab-content" class:active={tab === 2}>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+				eos tempore voluptatum quisquam saepe sed pariatur enim
+				quibusdam asperiores tempora earum quis aliquid repudiandae
+				dolores quaerat voluptate fugiat harum doloremque.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+				eos tempore voluptatum quisquam saepe sed pariatur enim
+				quibusdam asperiores tempora earum quis aliquid repudiandae
+				dolores quaerat voluptate fugiat harum doloremque.
+			</p>
+		</div>
 	</div>
 </div>
 
@@ -25,39 +80,78 @@
 		box-sizing: border-box;
 		padding: global.$margin-1 * 2 global.$margin-1 0 global.$margin-1 * 7/3;
 
-		.l {
-			width: calc(40vw - global.$margin-1);
+		.title {
+			font-size: global.$title-font-size;
+			margin-bottom: 8px;
+		}
 
-			.title {
-				font-size: global.$title-font-size;
-				margin-bottom: 8px;
-			}
-
-			.desc {
-				font-size: 20px;
-			}
+		.desc {
+			font-size: 20px;
+			padding: 0 0 global.$margin-1 0;
 		}
 
 		.project-list {
 			display: flex;
-			flex-direction: row;
+			flex-direction: column;
 			flex: 1 1 auto;
-			padding: 0 global.$margin-1;
 
-			.item {
-				margin-right: 8px;
-				background-color: global.$button-background;
-				width: 360px;
-				height: global.$top-bar-height;
+			// .tab {
+			// 	margin-right: 8px;
+			// 	background-color: global.$button-background;
+			// 	width: 360px;
+			// 	height: global.$top-bar-height;
+			// 	display: flex;
+			// 	align-items: center;
+			// 	justify-content: center;
+			// 	font-size: 24px;
+			// 	clip-path: polygon(0 0, 95% 0, 100% 30%, 100% 100%, 0 100%);
+			// }
+
+			.tabs {
 				display: flex;
-				align-items: center;
-				justify-content: center;
-				font-size: 16px;
-				clip-path: polygon(0 0, 95% 0, 100% 30%, 100% 100%, 0 100%);
+				flex-direction: row;
 
-				&:last-child {
-					margin-right: 0px;
+				.tab {
+					padding: 10px 20px;
+					margin-right: 5px;
+					border-radius: 5px 5px 0 0;
+					font-size: 24px;
+					background-color: global.$button-background;
+					clip-path: polygon(0 0, 95% 0, 100% 30%, 100% 100%, 0 100%);
+
+					@include global.flex-center;
+
+					button {
+						background: none;
+						border: none;
+						color: inherit;
+						width: 100%;
+						height: 100%;
+						cursor: pointer;
+					}
 				}
+				.tab.active {
+					// border-bottom: 2px solid #1954ec;
+
+					background: linear-gradient(
+						90deg,
+						rgba(199, 172, 255, 0.6),
+						rgba(255, 255, 255, 0.6)
+					);
+				}
+			}
+
+			.tab-content {
+				display: none;
+				padding: global.$margin-1 0 0 0;
+				flex: 1 1 auto;
+				width: 90%;
+				font-size: 24px;
+
+				@include global.galssblur(2px);
+			}
+			.tab-content.active {
+				display: block;
 			}
 		}
 	}
